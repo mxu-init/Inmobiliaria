2c8486719ee6b1ceb513166367934e20ff5fa1f3
mxu-init <14923949+mxu-init@users.noreply.github.com>
2026-08-17 13:58:03 +0200

    feature: convert sections to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import './style/App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import Apartaments from './components/apartaments/Apartaments';
-import Welcome from './components/welcome/Welcome';
-import History from './components/history/History';
-import Agentes from './components/agents/Agentes';
+import Apartaments from './pages/apartaments/Apartaments';
+import Welcome from './pages/welcome/Welcome';
+import History from './pages/history/History';
+import Agentes from './pages/agents/Agentes';
 // PENDIENTE DE COMPONENTES AGENTES Y RECETAS, SE DEBE PONER TANTO EN IMPORT COMO EN ROUTES
 
 
 function App() {
     return (
-        <BrowserRouter>
+        
             <div className="app" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
                 <Header />
                 <main style={{ flex: 1, marginTop: '80px' }}>
@@ -20,14 +20,12 @@ function App() {
                         <Route path="/pisos" element={<Apartaments />} />
                         <Route path="/historia" element={<History />} />
                         <Route path="/agentes" element={<Agentes />} />
-                        // PENDIENTE DE COMPONENTES AGENTES Y RECETAS, SE DEBE PONER TANTO EN IMPORT COMO EN ROUTES
-                        {/* <Route path="/agentes" element={<Agents />} /> */}
                         {/* <Route path="/recetas" element={<Recetas />} /> */}
                     </Routes>
                 </main>
                 <Footer />
             </div>
-        </BrowserRouter>
+        
     );
 }
 
