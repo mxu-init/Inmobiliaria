d310ca90652f84fcc1e3815c4fa2aac75bd85c51
mxu-init <14923949+mxu-init@users.noreply.github.com>
2026-08-18 09:38:30 +0200

    fix: separet apartmentCards into a component from apartment page

--- src/pages/apartaments/Apartaments.jsx
+++ src/pages/apartaments/Apartaments.jsx
@@ -1,33 +1,5 @@
 import "./Apartaments.css";
-
-const images = import.meta.glob("../../assets/img/*.png", {
-    eager: true,
-    import: "default",
-});
-
-function Card(props) {
-    return (
-        <div className="card">
-            <div className="cardImage">
-                <img
-                    className="cardImg"
-                    src={images[`../../assets/img/${props.image}.png`]}
-                    alt={props.title}
-                />
-                <span className="cardPrice">{props.price} </span>
-            </div>
-            <div className="cardInfo">
-                <p className="cardArea">{props.area}</p>
-                <h3 className="cardTitle">{props.title}</h3>
-                <div className="cardDetails">
-                    <span>{props.rooms} Hab.</span>
-                    <span>{props.bathrooms} Baños</span>
-                    <span>{props.extra}</span>
-                </div>
-            </div>
-        </div>
-    );
-}
+import Card from "../../components/ApartmentCards";
 
 function Apartaments() {
     return (
